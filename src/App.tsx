a5fa2038e90d0b57c1cb316325227e80ffa43e45
Vladymyr Radionov <radionovdon@gmail.com>
2022-05-04 19:05:20 +0300

    some first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Footer } from "./Footer";
+import info from "./utils/config/info.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const user = {
+//   username: "Bot",
+// };
+
+const App = () => (
+  <div className="App">
+    <h1>H1 {info.User.first_name}</h1>
+    <p>You {info.User.short_bio}</p>
+    <p>Your contacts: </p>
+    <ul>
+      {info.User.public_contacts.map((item, i) => (
+        <li key={i}>
+          <a href="#contact" target="_blank">
+            {item}
+          </a>
+        </li>
+      ))}
+    </ul>
+    <Footer copyright="C" />
+  </div>
+);
 
 export default App;
